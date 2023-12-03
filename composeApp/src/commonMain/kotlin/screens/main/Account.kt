1b49246e3c27cefc4d884c9d87f29423c862872a
damilola <damilolaakinterinwa@gmail.com>
2023-12-03 12:58:49 +0100

    Create User Account Action Items

--- composeApp/src/commonMain/kotlin/screens/main/Account.kt
+++ composeApp/src/commonMain/kotlin/screens/main/Account.kt
@@ -3,7 +3,11 @@ package screens.main
 
 import AppTheme.AppBoldTypography
 import AppTheme.AppColors
+import AppTheme.AppRegularTypography
+import AppTheme.AppSemiBoldTypography
 import GGSansBold
+import GGSansRegular
+import GGSansSemiBold
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -16,9 +20,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -35,6 +43,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -43,10 +52,12 @@ import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import components.ButtonComponent
 import components.GradientButton
+import components.IconButtonComponent
 import components.ImageComponent
 import components.TextComponent
 import screens.SplashScreenCompose
 import screens.authentication.AuthenticationComposeScreen
+import screens.authentication.attachAuthenticationButton
 import screens.authentication.attachWaveIcon
 
 class AccountTab(private val mainViewModel: MainViewModel) : Tab {
@@ -73,12 +84,11 @@ class AccountTab(private val mainViewModel: MainViewModel) : Tab {
     override fun Content() {
         mainViewModel.setTitle(options.title.toString())
         val columnModifier = Modifier
-            .background(color = Color(0xFBFBFB))
             .padding(top = 5.dp)
             .fillMaxHeight()
             .fillMaxWidth()
 
-        MaterialTheme(colors = AppColors(), typography = AppBoldTypography()) {
+        MaterialTheme(colors = AppColors(), typography = AppRegularTypography()) {
             Column(
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -87,6 +97,10 @@ class AccountTab(private val mainViewModel: MainViewModel) : Tab {
                     attachUserProfileImage()
                     UserFullNameComp()
                     EditProfileComp(MaterialTheme.typography.button)
+
+                Divider(color = Color(color = 0x90C8C8C8), thickness = 2.dp, modifier = Modifier.fillMaxWidth(0.90f).padding(top = 30.dp))
+
+                attachAccountAction()
             }
         }
     }
@@ -121,7 +135,7 @@ class AccountTab(private val mainViewModel: MainViewModel) : Tab {
             ) {
                 val modifier = Modifier.padding(start = 5.dp)
                 TextComponent(
-                    text = "Tessy Ogechi Iwunze",
+                    text = "My Name is here",
                     fontSize = 23,
                     fontFamily = GGSansBold,
                     textStyle = MaterialTheme.typography.h6,
@@ -141,10 +155,163 @@ class AccountTab(private val mainViewModel: MainViewModel) : Tab {
 
         val buttonStyle = Modifier
             .padding(top = 15.dp)
-            .fillMaxWidth(0.45f)
-            .height(50.dp)
+            .fillMaxWidth(0.55f)
+            .height(55.dp)
 
-        ButtonComponent(modifier = buttonStyle, buttonText = "Edit Profile", borderStroke = null, colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray), fontSize = 16, shape = RoundedCornerShape(12.dp), textColor = Color.White, style = style){}
+        ButtonComponent(modifier = buttonStyle, buttonText = "Edit Profile", borderStroke = null, colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray), fontSize = 20, shape = RoundedCornerShape(12.dp), textColor = Color.White, style = style){}
 
     }
+
+
+    @OptIn(ExperimentalMaterialApi::class)
+    @Composable
+    public fun AccountActionButtonComponent(modifier: Modifier, buttonText: String, colors: ButtonColors, textColor: Color, fontSize: Int, style: TextStyle, iconRes: String, onClick: (() -> Unit)? = null) {
+        val rowModifier = Modifier
+            .fillMaxWidth()
+        MaterialTheme(colors = AppColors(), typography = AppRegularTypography()) {
+
+
+            Button(
+                onClick = {
+                    if (onClick != null) {
+                        onClick()
+                    }
+                },
+                modifier = modifier,
+                colors = colors,
+                elevation = ButtonDefaults.elevation(
+                    defaultElevation = 0.dp,
+                    pressedElevation = 0.dp,
+                    disabledElevation = 0.dp
+                )
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.Top,
+                    modifier = rowModifier
+                ) {
+
+                    val iconModifier = Modifier
+                        .padding(top = 5.dp)
+                        .size(24.dp)
+
+                    val iconTextBoxModifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth(0.90f)
+
+                    val iconNavModifier = Modifier
+                        .fillMaxWidth()
+                        .size(28.dp)
+
+                    val textModifier = Modifier
+                        .padding(top = 2.dp, end = 5.dp, start = 20.dp)
+                        .wrapContentSize()
+                    Row(
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.Top,
+                        modifier = iconTextBoxModifier
+                    ) {
+                        ImageComponent(imageModifier = iconModifier, imageRes = iconRes)
+                        TextComponent(
+                            text = buttonText,
+                            fontSize = fontSize,
+                            textStyle = style,
+                            textColor = textColor,
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = GGSansSemiBold,
+                            textModifier = textModifier
+                        )
+                    }
+
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.Top,
+                        modifier = iconNavModifier
+                    ) {
+                        ImageComponent(imageModifier = iconModifier, imageRes = "chevron_right.png")
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    fun attachAccountAction() {
+        val columnModifier = Modifier
+            .padding(top = 20.dp, start = 5.dp)
+            .fillMaxWidth()
+
+        val buttonStyle = Modifier
+            .padding(bottom = 5.dp)
+            .fillMaxWidth()
+            .height(50.dp)
+
+        MaterialTheme(colors = AppColors(), typography = AppRegularTypography()) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start,
+                modifier = columnModifier
+            ) {
+
+                AccountActionButtonComponent(
+                    modifier = buttonStyle,
+                    buttonText = "Settings",
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                    fontSize = 20,
+                    textColor = Color.DarkGray,
+                    style = MaterialTheme.typography.button,
+                    iconRes = "settings_icon.png"
+                ) {}
+
+
+                AccountActionButtonComponent(
+                    modifier = buttonStyle,
+                    buttonText = "My Bookings",
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                    fontSize = 20,
+                    textColor = Color.DarkGray,
+                    style = MaterialTheme.typography.button,
+                    iconRes = "booking_icon.png"
+                ) {}
+
+                AccountActionButtonComponent(
+                    modifier = buttonStyle,
+                    buttonText = "Address",
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                    fontSize = 20,
+                    textColor = Color.DarkGray,
+                    style = MaterialTheme.typography.button,
+                    iconRes = "location_icon.png"
+                ) {}
+
+                Divider(color = Color(color = 0x90C8C8C8), thickness = 2.dp, modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp, bottom = 20.dp))
+
+
+                AccountActionButtonComponent(
+                    modifier = buttonStyle,
+                    buttonText = "Help & Support",
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                    fontSize = 20,
+                    textColor = Color.DarkGray,
+                    style = MaterialTheme.typography.button,
+                    iconRes = "help_circle_icon.png"
+                ) {}
+
+                AccountActionButtonComponent(
+                    modifier = buttonStyle,
+                    buttonText = "Log out",
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                    fontSize = 20,
+                    textColor = Color.DarkGray,
+                    style = MaterialTheme.typography.button,
+                    iconRes = "logout_icon.png"
+                ) {}
+
+            }
+        }
+    }
+
 }
