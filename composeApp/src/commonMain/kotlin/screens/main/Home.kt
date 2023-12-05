e2a47c10193632c50e99443a8de8874572897798
damilola <damilolaakinterinwa@gmail.com>
2023-12-05 17:35:18 +0100

    Update TopNav

--- composeApp/src/commonMain/kotlin/screens/main/Home.kt
+++ composeApp/src/commonMain/kotlin/screens/main/Home.kt
@@ -2,6 +2,8 @@ package screens.main
 
 import AppTheme.AppBoldTypography
 import AppTheme.AppColors
+import GGSansBold
+import GGSansSemiBold
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -28,12 +30,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import components.IconButtonComponent
 import components.ImageComponent
+import components.TextComponent
 import screens.SplashScreenCompose
+import screens.authentication.attachWaveIcon
 
 class HomeTab(private val mainViewModel: MainViewModel) : Tab {
 
@@ -57,8 +64,6 @@ class HomeTab(private val mainViewModel: MainViewModel) : Tab {
 
     @Composable
     override fun Content() {
-
-
         mainViewModel.setTitle(options.title.toString())
         val columnModifier = Modifier
             .padding(top = 5.dp)
@@ -78,9 +83,48 @@ class HomeTab(private val mainViewModel: MainViewModel) : Tab {
                         .fillMaxWidth()
                         .background(color = Color(0xFFF3F3F3)),
                     horizontalArrangement = Arrangement.Center
-                ) {}
+                ) {
+                    attachUserAddress()
+                }
 
             }
         }
+    }
+
+    @Composable
+    fun attachUserAddress(){
+        val rowModifier = Modifier
+            .padding(start = 20.dp)
+            .fillMaxWidth()
+        MaterialTheme(colors = AppColors(), typography = AppBoldTypography()) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.Top,
+                modifier = rowModifier
+            ) {
+                val modifier = Modifier.padding(start = 3.dp)
+                attachLocationIcon()
+                TextComponent(
+                    text = "301 Dorthy Walks, Chicago, Illinois",
+                    fontSize = 16,
+                    fontFamily = GGSansBold,
+                    textStyle = MaterialTheme.typography.h6,
+                    textColor = Color.DarkGray,
+                    textAlign = TextAlign.Left,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 30,
+                    textModifier = modifier
+                )
+            }
+        }
+    }
+
+
+    @Composable
+    fun attachLocationIcon() {
+        val modifier = Modifier
+            .padding(top = 2.dp)
+            .size(18.dp)
+        ImageComponent(imageModifier = modifier, imageRes = "location_icon_filled.png", colorFilter = ColorFilter.tint(color = Color.LightGray))
     }
 }
