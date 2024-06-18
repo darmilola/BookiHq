e7c392715e9b3157bc0799fca839862ae88f1ca5
damilola <damilolaakinterinwa@gmail.com>
2024-06-18 15:27:13 +0100

    Fix Model error

--- composeApp/src/commonMain/kotlin/domain/Models/Appointment.kt
+++ composeApp/src/commonMain/kotlin/domain/Models/Appointment.kt
@@ -10,7 +10,7 @@ data class Appointment(
     @SerialName("id") val appointmentId: Int? = -1, @SerialName("user_id") val userId: Int? = -1, @SerialName("vendor_id") val vendorId: Int = -1,
     @SerialName("serviceLocation") val serviceLocation: String = ServiceLocation.Spa.toPath(), @SerialName("serviceStatus") val serviceStatus: String = ServiceStatus.Pending.toPath(), @SerialName("service_id") val serviceId: Int = -1,
     @SerialName("specialist_id") val specialistId: Int = -1, @SerialName("service_type_id") val serviceTypeId: Int? = -1, @SerialName("appointmentTime") val appointmentTime: Int? = -1,
-    @SerialName("day") val appointmentDay: Int? = -1, @SerialName("month") val appointmentMonth: Int? = -1, val appointmentType: String = AppointmentType.SERVICE.toPath(),
+    @SerialName("day") val appointmentDay: Int? = -1, @SerialName("month") val appointmentMonth: Int? = -1, @SerialName("appointmentType") val appointmentType: String = AppointmentType.SERVICE.toPath(),
     @SerialName("year") val appointmentYear: Int? = -1, @SerialName("isRecommendedAppointment") val isRecommendedAppointment: Boolean = false,
     @SerialName("recommendation_id") val recommendationId: Int? = -1, @SerialName("specialist_info") val specialistInfo: SpecialistInfo? = null,
     @SerialName("service_type") val serviceTypeItem: ServiceTypeItem? = null, @SerialName("service") val services: Services? = null, @SerialName("time") val platformTime: PlatformTime? = null,
