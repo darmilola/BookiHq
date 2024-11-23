917e70c4251159faccc4182841e3e1e5a0f2a070
damilola <damilolaakinterinwa@gmail.com>
2024-11-23 16:35:14 +0100

    Update UI

--- composeApp/src/commonMain/kotlin/domain/Enums/Keys.kt
+++
@@ -1,8 +0,0 @@
-package domain.Enums
-
-enum class Keys {
-    PAYSTACK_PUBLIC_KEY;
-    fun toPath() = when (this) {
-        PAYSTACK_PUBLIC_KEY -> "pk_test_bf395e89a315198929ddca163fafecf64899d524"
-    }
-}
