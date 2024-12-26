c5aa4ba3fb48c7c83d3db8b7a1bb8ab8bde83219
damilola <damilolaakinterinwa@gmail.com>
2024-12-26 21:57:52 +0100

    Updates

--- composeApp/src/commonMain/kotlin/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/di/Koin.kt
@@ -48,7 +48,7 @@ private val KtorModule = module {
         httpClient {
             defaultRequest {
                 url {
-                    host = "/devprocess-xxqg.onrender.com/api/v1"
+                    host = "/zappyapp-server.onrender.com/api/v1"
                     protocol = URLProtocol.HTTPS
                     port = 443
                 }
