d1f19ad1dcf3cf4a10e408ffc61a5271f1d90f0a
damilola <damilolaakinterinwa@gmail.com>
2025-05-14 14:06:38 +0100

    Updates

--- composeApp/src/commonMain/kotlin/theme/Colors.kt
+++ composeApp/src/commonMain/kotlin/theme/Colors.kt
@@ -4,11 +4,11 @@ import androidx.compose.ui.graphics.Color
 
 
 object Colors {
-    val primaryColor: Color = Color(color = 0xFF03C3F7)
+    val primaryColor: Color = Color(color = 0xFF61A7B8)
     val transparentPrimaryColor: Color = Color(color = 0x80004659)
     val lightPrimaryColor: Color = Color(color = 0xFFE6F8FF)
     val surfaceColor: Color = Color(color = 0xFFF5FAFC)
-    val darkPrimary: Color = Color(color = 0xFF004659)
+    val darkPrimary: Color = Color(color = 0xFF053549)
     val pinkColor: Color = Color(color = 0xFFFA2D65)
     val greenColor: Color = Color(color = 0xFF1f9978)
     val yellowColor: Color = Color(color = 0xFFff8E1B)
