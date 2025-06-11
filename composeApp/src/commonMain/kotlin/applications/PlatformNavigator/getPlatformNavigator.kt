cb1d259800be03319fcab8e832ca3524109d99b9
damilola <damilolaakinterinwa@gmail.com>
2025-06-11 12:31:09 +0100

    Updates

---
+++ composeApp/src/commonMain/kotlin/applications/PlatformNavigator/getPlatformNavigator.kt
@@ -0,0 +1,7 @@
+package applications.PlatformNavigator
+
+import androidx.compose.runtime.Composable
+import domain.Models.PlatformNavigator
+import domain.Models.ScreenSizeInfo
+
+expect fun platformNavigator(): PlatformNavigator
