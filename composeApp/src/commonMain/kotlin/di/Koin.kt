dfb31bc741a418d601c02c521ee598cb46aa889f
damilola <damilolaakinterinwa@gmail.com>
2025-06-13 16:03:20 +0100

    Updates

--- composeApp/src/commonMain/kotlin/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/di/Koin.kt
@@ -48,7 +48,7 @@ private val KtorModule = module {
         httpClient {
             defaultRequest {
                 url {
-                    host = "/yesbeauty-wy9j.onrender.com/api/v1"
+                    host = "/yesbeauty-hr1y.onrender.com/api/v1"
                     protocol = URLProtocol.HTTPS
                     port = 443
                 }
