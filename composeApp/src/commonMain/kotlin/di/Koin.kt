c60e8bbf6c7ac763c25daae564550ad31fae9e4e
damilola <damilolaakinterinwa@gmail.com>
2025-08-06 19:32:22 +0100

    Database Migration

--- composeApp/src/commonMain/kotlin/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/di/Koin.kt
@@ -48,7 +48,7 @@ private val KtorModule = module {
         httpClient {
             defaultRequest {
                 url {
-                    host = "/booki-0kc3.onrender.com/api/v1"
+                    host = "/bookihq-backend.onrender.com/api/v1"
                     protocol = URLProtocol.HTTPS
                     port = 443
                 }
